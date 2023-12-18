4c16c8261bd99e20a830df6bf45a9ba8be16be79
filbertfederico <filbertblack@gmail.com>
2023-12-19 02:53:33 +0700

    update output comparison

--- PatternFinding.py
+++ PatternFinding.py
@@ -31,41 +31,58 @@ def select_gene_sequence(input_gene):
             print("Random file: ", random_file)
         elif reg_input == 2:
             gene_sequence = "hooman/haplogroup HV-T16311C.txt"
+            print(gene_sequence)
         elif reg_input == 3:
             gene_sequence = "hooman/haplogroup I1c.txt"
+            print(gene_sequence)
         elif reg_input == 4:
             gene_sequence = "hooman/haplogroup K1a.txt"
+            print(gene_sequence)
         elif reg_input == 5:
             gene_sequence = "hooman/haplogroup U3a.txt"
+            print(gene_sequence)
         elif reg_input == 6:
             gene_sequence = "hooman/haplogroup K2b1.txt"
+            print(gene_sequence)
         elif reg_input == 7:
             gene_sequence = "hooman/haplogroup T2b.txt"
+            print(gene_sequence)
         elif reg_input == 8:
             gene_sequence = "hooman/haplogroup D1g.txt"
+            print(gene_sequence)
         elif reg_input == 9:
             gene_sequence = "hooman/haplogroup R0a2m.txt"
+            print(gene_sequence)
         elif reg_input == 10:
             gene_sequence = "hooman/homoSapiensGene.txt"
+            print(gene_sequence)
         elif reg_input == 11:
             gene_sequence = "gene/adenomatous polyposis coli.txt"
+            print(gene_sequence)
         elif reg_input == 12:
             gene_sequence = "gene/antithrombin.txt"
+            print(gene_sequence)
         elif reg_input == 13:
             gene_sequence = "gene/Apolipoprotein E.txt"
+            print(gene_sequence)
         elif reg_input == 14:
             gene_sequence = "gene/Cystic fibrosis.txt"
+            print(gene_sequence)
         elif reg_input == 15:
             gene_sequence = "gene/hemophilia A.txt"
+            print(gene_sequence)
         elif reg_input == 16:
             gene_sequence = "gene/hemophilia B.txt"
+            print(gene_sequence)
         elif reg_input == 17:
             gene_sequence = "gene/insulin receptor.txt"
+            print(gene_sequence)
         elif reg_input == 18:
             gene_sequence = "gene/lipoprotein lipase.txt"
+            print(gene_sequence)
         elif reg_input == 19:
             gene_sequence = "gene/steroid 21-hydroxylase.txt"
-    
+            print(gene_sequence)
         else:
             print("Invalid input")
 
@@ -79,77 +96,96 @@ def select_gene_sequence(input_gene):
 # Assuming you call the function somewhere in your code
 select_gene_sequence(input_gene)
 
-def find_patterns(gene_sequence, patterns):
+def find_patterns_all(gene_sequence, patterns):
+    with open(gene_sequence, 'r') as file:
+        gene_sequence = file.read().replace('\n', '')
+    total_length = len(gene_sequence)
+    results_all = {}
+
+    for pattern in patterns:
+        matches = re.findall(pattern, gene_sequence)
+        match_length = len(matches)
+        percentage = (match_length / total_length) * 100
+        expected_percentage = expected_results.get(pattern, {}).get('percentage', 0)
+
+        results_all[pattern] = {
+            'matches': matches,
+            'percentage': percentage,
+            'expected_percentage' : expected_percentage
+        }
+    return results_all
+expected_results = {
+    'TTT': {'matches': 192, 'percentage': 3.476371537208039},
+    'TTC': {'matches': 308, 'percentage': 5.576679340937896},
+    'TTA': {'matches': 329, 'percentage': 5.9569074778200255},
+    'TTG': {'matches': 116, 'percentage': 2.100307803729857},
+    'TCT': {'matches': 288, 'percentage': 5.2145573058120585},
+    'TCC': {'matches': 361, 'percentage': 6.5363027340213655},
+    'TCA': {'matches': 415, 'percentage': 7.514032228861126},
+    'TCG': {'matches': 121, 'percentage': 2.190838312511316},
+    'TAT': {'matches': 304, 'percentage': 5.504254933912729},
+    'TAC': {'matches': 377, 'percentage': 6.826000362122035},
+    'TAA': {'matches': 414, 'percentage': 7.495926127104835},
+    'TAG': {'matches': 258, 'percentage': 4.671374253123303},
+    'TGT': {'matches': 99, 'percentage': 1.7925040738728952},
+    'TGC': {'matches': 123, 'percentage': 2.2270505160239003},
+    'TGA': {'matches': 190, 'percentage': 3.4401593336954557},
+    'TGG': {'matches': 99, 'percentage': 1.7925040738728952},
+    'CTT': {'matches': 318, 'percentage':5.757740358500815},
+    'CTC': {'matches': 395, 'percentage':7.151910193735289},
+    'CTA': {'matches': 523, 'percentage':9.469491218540648},
+    'CTG': {'matches': 180, 'percentage':3.259098316132537},
+    'CCT': {'matches': 542, 'percentage':9.813507151910192},
+    'CCC': {'matches': 411, 'percentage':7.441607821835959},
+    'CCA': {'matches': 464, 'percentage':8.401231214919427},
+    'CCG': {'matches': 141, 'percentage':2.552960347637154},
+    'CAT': {'matches': 416, 'percentage':7.532138330617418},
+    'CAC': {'matches': 418, 'percentage':7.568350534130001},
+    'CAA': {'matches': 465, 'percentage':8.419337316675719},
+    'CAG': {'matches': 199, 'percentage':3.6031142495020827},
+    'CGT': {'matches': 78, 'percentage':1.4122759369907658},
+    'CGC': {'matches': 155, 'percentage':2.80644577222524},
+    'CGA': {'matches': 122, 'percentage':2.2089444142676085},
+    'CGG': {'matches': 80, 'percentage':1.4484881405033496},
+    'ATT': {'matches': 330, 'percentage': 5.975013579576317},
+    'ATC': {'matches': 371, 'percentage': 6.7173637515842834},
+    'ATA': {'matches': 348, 'percentage': 6.300923411189571},
+    'ATG': {'matches': 162, 'percentage': 2.9331884845192833},
+    'ACT': {'matches': 412, 'percentage': 7.459713923592251},
+    'ACC': {'matches': 515, 'percentage': 9.324642404490312},
+    'ACA': {'matches': 409, 'percentage': 7.405395618323375},
+    'ACG': {'matches': 119, 'percentage': 2.1546261089987326},
+    'AAT': {'matches': 376, 'percentage': 6.807894260365743},
+    'AAC': {'matches': 495, 'percentage': 8.962520369364475},
+    'AAA': {'matches': 361, 'percentage': 6.5363027340213655},
+    'AAG': {'matches': 209, 'percentage': 3.7841752670650006},
+    'AGT': {'matches': 161, 'percentage': 2.915082382762991},
+    'AGC': {'matches': 282, 'percentage': 5.105920695274308},
+    'AGA': {'matches': 173, 'percentage': 3.1323556038384934},
+    'AGG': {'matches': 174, 'percentage': 3.1504617055947857},
+    'GTT': {'matches': 104, 'percentage': 1.8830345826543544},
+    'GTC': {'matches': 106, 'percentage': 1.9192467861669382},
+    'GTA': {'matches': 154, 'percentage': 2.788339670468948},
+    'GTG': {'matches': 53, 'percentage': 0.9596233930834691},
+    'GCT': {'matches': 179, 'percentage': 3.2409922143762446},
+    'GCC': {'matches': 271, 'percentage': 4.906753575955096},
+    'GCA': {'matches': 207, 'percentage': 3.7479630635524175},
+    'GCG': {'matches': 52, 'percentage': 0.9415172913271772},
+    'GAT': {'matches': 114, 'percentage': 2.0640956002172732},
+    'GAC': {'matches': 169, 'percentage': 3.0599311968133263},
+    'GAA': {'matches': 201, 'percentage': 3.639326453014666},
+    'GAG': {'matches': 126, 'percentage': 2.2813688212927756},
+    'GGT': {'matches': 80, 'percentage': 1.4484881405033496},
+    'GGC': {'matches': 151, 'percentage': 2.734021365200072},
+    'GGA': {'matches': 122, 'percentage': 2.2089444142676085},
+    'GGG': {'matches': 58, 'percentage': 1.0501539018649284}
+}
+
+def find_patterns_with_comparison(gene_sequence, patterns):
     with open(gene_sequence, 'r') as file:
         gene_sequence = file.read()
 
     total_length = len(gene_sequence)
-    expected_results = {
-        'TTT': {'matches': 192, 'percentage': 3.476371537208039},
-        'TTC': {'matches': 308, 'percentage': 5.576679340937896},
-        'TTA': {'matches': 329, 'percentage': 5.9569074778200255},
-        'TTG': {'matches': 116, 'percentage': 2.100307803729857},
-        'TCT': {'matches': 288, 'percentage': 5.2145573058120585},
-        'TCC': {'matches': 361, 'percentage': 6.5363027340213655},
-        'TCA': {'matches': 415, 'percentage': 7.514032228861126},
-        'TCG': {'matches': 121, 'percentage': 2.190838312511316},
-        'TAT': {'matches': 304, 'percentage': 5.504254933912729},
-        'TAC': {'matches': 377, 'percentage': 6.826000362122035},
-        'TAA': {'matches': 414, 'percentage': 7.495926127104835},
-        'TAG': {'matches': 258, 'percentage': 4.671374253123303},
-        'TGT': {'matches': 99, 'percentage': 1.7925040738728952},
-        'TGC': {'matches': 123, 'percentage': 2.2270505160239003},
-        'TGA': {'matches': 190, 'percentage': 3.4401593336954557},
-        'TGG': {'matches': 99, 'percentage': 1.7925040738728952},
-        'CTT': {'matches': 318, 'percentage':5.757740358500815},
-        'CTC': {'matches': 395, 'percentage':7.151910193735289},
-        'CTA': {'matches': 523, 'percentage':9.469491218540648},
-        'CTG': {'matches': 180, 'percentage':3.259098316132537},
-        'CCT': {'matches': 542, 'percentage':9.813507151910192},
-        'CCC': {'matches': 411, 'percentage':7.441607821835959},
-        'CCA': {'matches': 464, 'percentage':8.401231214919427},
-        'CCG': {'matches': 141, 'percentage':2.552960347637154},
-        'CAT': {'matches': 416, 'percentage':7.532138330617418},
-        'CAC': {'matches': 418, 'percentage':7.568350534130001},
-        'CAA': {'matches': 465, 'percentage':8.419337316675719},
-        'CAG': {'matches': 199, 'percentage':3.6031142495020827},
-        'CGT': {'matches': 78, 'percentage':1.4122759369907658},
-        'CGC': {'matches': 155, 'percentage':2.80644577222524},
-        'CGA': {'matches': 122, 'percentage':2.2089444142676085},
-        'CGG': {'matches': 80, 'percentage':1.4484881405033496},
-        'ATT': {'matches': 330, 'percentage': 5.975013579576317},
-        'ATC': {'matches': 371, 'percentage': 6.7173637515842834},
-        'ATA': {'matches': 348, 'percentage': 6.300923411189571},
-        'ATG': {'matches': 162, 'percentage': 2.9331884845192833},
-        'ACT': {'matches': 412, 'percentage': 7.459713923592251},
-        'ACC': {'matches': 515, 'percentage': 9.324642404490312},
-        'ACA': {'matches': 409, 'percentage': 7.405395618323375},
-        'ACG': {'matches': 119, 'percentage': 2.1546261089987326},
-        'AAT': {'matches': 376, 'percentage': 6.807894260365743},
-        'AAC': {'matches': 495, 'percentage': 8.962520369364475},
-        'AAA': {'matches': 361, 'percentage': 6.5363027340213655},
-        'AAG': {'matches': 209, 'percentage': 3.7841752670650006},
-        'AGT': {'matches': 161, 'percentage': 2.915082382762991},
-        'AGC': {'matches': 282, 'percentage': 5.105920695274308},
-        'AGA': {'matches': 173, 'percentage': 3.1323556038384934},
-        'AGG': {'matches': 174, 'percentage': 3.1504617055947857},
-        'GTT': {'matches': 104, 'percentage': 1.8830345826543544},
-        'GTC': {'matches': 106, 'percentage': 1.9192467861669382},
-        'GTA': {'matches': 154, 'percentage': 2.788339670468948},
-        'GTG': {'matches': 53, 'percentage': 0.9596233930834691},
-        'GCT': {'matches': 179, 'percentage': 3.2409922143762446},
-        'GCC': {'matches': 271, 'percentage': 4.906753575955096},
-        'GCA': {'matches': 207, 'percentage': 3.7479630635524175},
-        'GCG': {'matches': 52, 'percentage': 0.9415172913271772},
-        'GAT': {'matches': 114, 'percentage': 2.0640956002172732},
-        'GAC': {'matches': 169, 'percentage': 3.0599311968133263},
-        'GAA': {'matches': 201, 'percentage': 3.639326453014666},
-        'GAG': {'matches': 126, 'percentage': 2.2813688212927756},
-        'GGT': {'matches': 80, 'percentage': 1.4484881405033496},
-        'GGC': {'matches': 151, 'percentage': 2.734021365200072},
-        'GGA': {'matches': 122, 'percentage': 2.2089444142676085},
-        'GGG': {'matches': 58, 'percentage': 1.0501539018649284}
-    }
 
     results = {}
     for pattern in patterns:
@@ -188,7 +224,10 @@ patterns = ['TTT', 'TTC', 'TTA', 'TTG',
             'GGT', 'GGC', 'GGA', 'GGG']  # These are the patterns we're looking for
 
 # Print comparison at the beginning
-results = find_patterns(gene_sequence, patterns)
-for pattern, result in results.items():
-    print(f"Pattern: {pattern}, Found {len(result['matches'])} match(es), Percentage: {result['percentage']}%")
-    print(f"Expected Percentage: {result['expected_percentage']}/n")
+results_comparison = find_patterns_with_comparison(gene_sequence, patterns)
+for pattern, result in results_comparison.items():
+    print(f"Pattern: {pattern}, Found {len(result['matches'])} match(es), Percentage: {result['percentage']}%, Expected Percentage: {result['expected_percentage']}")
+print("--------------------------------------------------------------------")
+results_all = find_patterns_all(gene_sequence, patterns)
+for pattern, result in results_all.items():
+    print(f"Pattern: {pattern}, Found {len(result['matches'])} match(es), Percentage: {result['percentage']}%, Expected Percentage: {result['expected_percentage']}")
